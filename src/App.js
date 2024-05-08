6b73efdec626a15329d7cb9df55f2432c71a6257
Black-Kaleigh <BLACKTAMARAKALEIGH@GMAIL.COM>
2024-05-08 12:04:43 +0200

    added github url

--- src/App.js
+++ src/App.js
@@ -3,5 +3,35 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/black-kaleigh"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Black Kaleigh{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/Black-Kaleigh/Kaleigh-Vanilla-weather-app"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          open sourced on GitHub{" "}
+        </a>
+        and hosted{" "}
+        <a
+          href="https://app.netlify.com/sites/frabjous-empanada-8965ad/overview"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
